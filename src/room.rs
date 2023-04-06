53d40ed0ed7ec6aed73ac2c0de43b2d900f259fc
Jozef <jabczunjozef@gmail.com>
2023-04-06 12:58:13 +0200

    fix: arena doesn't spawn twice on a second create room event

--- src/room.rs
+++ src/room.rs
@@ -40,46 +40,53 @@ pub struct SpawnRoomEvent {
 
 const FLOOR_COLOR: Color = Color::DARK_GRAY;
 
+#[derive(Component, Clone, Default, Debug)]
+struct Arena;
+
 fn spawn_rooms(
     mut commands: Commands,
     mut room_spawn_events: EventReader<SpawnRoomEvent>,
     mut spawn_enemy_events: EventWriter<SpawnEnemyEvent>,
+    arenas: Query<(), With<Arena>>,
 ) {
     for SpawnRoomEvent { room } in room_spawn_events.iter() {
         // Spawn static elements
-        commands.spawn((
-            RigidBody::Fixed,
-            Collider::compound(vec![
-                (
-                    Vec2::new(0.0, 8.0 * 16.0),
-                    0.0,
-                    Collider::cuboid(29.0 * 8.0, 1.0),
-                ),
-                (
-                    Vec2::new(0.0, -8.0 * 16.0),
-                    0.0,
-                    Collider::cuboid(29.0 * 8.0, 1.0),
-                ),
-                (
-                    Vec2::new(14.5 * 16.0, 0.0),
-                    0.0,
-                    Collider::cuboid(1.0, 16.0 * 8.0),
-                ),
-                (
-                    Vec2::new(-14.5 * 16.0, 0.0),
-                    0.0,
-                    Collider::cuboid(1.0, 16.0 * 8.0),
-                ),
-            ]),
-            SpriteBundle {
-                sprite: Sprite {
-                    color: FLOOR_COLOR,
-                    custom_size: Some(Vec2::new(29.0 * 16.0, 16.0 * 16.0)),
+        if arenas.iter().len() == 0 {
+            commands.spawn((
+                Arena,
+                RigidBody::Fixed,
+                Collider::compound(vec![
+                    (
+                        Vec2::new(0.0, 8.0 * 16.0),
+                        0.0,
+                        Collider::cuboid(29.0 * 8.0, 1.0),
+                    ),
+                    (
+                        Vec2::new(0.0, -8.0 * 16.0),
+                        0.0,
+                        Collider::cuboid(29.0 * 8.0, 1.0),
+                    ),
+                    (
+                        Vec2::new(14.5 * 16.0, 0.0),
+                        0.0,
+                        Collider::cuboid(1.0, 16.0 * 8.0),
+                    ),
+                    (
+                        Vec2::new(-14.5 * 16.0, 0.0),
+                        0.0,
+                        Collider::cuboid(1.0, 16.0 * 8.0),
+                    ),
+                ]),
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: FLOOR_COLOR,
+                        custom_size: Some(Vec2::new(29.0 * 16.0, 16.0 * 16.0)),
+                        ..Default::default()
+                    },
                     ..Default::default()
                 },
-                ..Default::default()
-            },
-        ));
+            ));
+        }
 
         // Spawn enemies
         let positions = [
@@ -105,7 +112,6 @@ fn check_room_cleared(
     let enemy_count = enemies.iter().len();
     if enemy_count == 0 && enemy_count != *last_enemy_count {
         room_clear_events.send_default();
-        info!("YOU WIN");
     }
     *last_enemy_count = enemy_count;
 }

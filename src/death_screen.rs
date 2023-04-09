bd0b193a646feb8f6017b991f9afa0d492cfb755
Jozef Jabczun <jabczunjozef@gmail.com>
2023-04-09 19:14:21 +0200

    add: change to main menu from the death screen after 3s passed

--- src/death_screen.rs
+++ src/death_screen.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use bevy::prelude::*;
 
 use crate::{assets::GameAssets, state::GameState};
@@ -6,16 +8,22 @@ pub struct DeathScreenPlugin;
 
 impl Plugin for DeathScreenPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(show_deathscreen.in_schedule(OnEnter(GameState::DeathScreen)));
+        app.add_system(show_deathscreen.in_schedule(OnEnter(GameState::DeathScreen)))
+            .add_system(exit_deathscreen.in_set(OnUpdate(GameState::DeathScreen)))
+            .add_system(cleanup.in_schedule(OnExit(GameState::DeathScreen)));
     }
 }
 
 #[derive(Component, Default, Debug)]
 struct DeathScreen;
 
+#[derive(Component, Default, Debug)]
+struct DeathScreenTimer(pub Timer);
+
 fn show_deathscreen(mut commands: Commands, assets: Res<GameAssets>) {
     commands.spawn((
         DeathScreen,
+        DeathScreenTimer(Timer::new(Duration::from_secs(3), TimerMode::Once)),
         TextBundle {
             text: Text::from_section(
                 "You have died!",
@@ -34,4 +42,22 @@ fn show_deathscreen(mut commands: Commands, assets: Res<GameAssets>) {
             ..Default::default()
         },
     ));
+}
+
+fn exit_deathscreen(
+    mut timers: Query<&mut DeathScreenTimer>,
+    time: Res<Time>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    for mut timer in &mut timers {
+        if timer.0.tick(time.delta()).just_finished() {
+            next_state.set(GameState::MainMenu);
+        }
+    }
+}
+
+fn cleanup(mut commands: Commands, death_screen: Query<Entity, With<DeathScreen>>) {
+    for e in &death_screen {
+        commands.entity(e).despawn_recursive();
+    }
 }

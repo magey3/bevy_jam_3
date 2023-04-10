3cd040b112a796160e9cecf40841752bec971cfc
Jozef Jabczun <jabczunjozef@gmail.com>
2023-04-10 14:18:23 +0200

    add: cooldown ui

--- src/ability_ui.rs
+++ src/ability_ui.rs
@@ -1,13 +1,18 @@
 use bevy::prelude::*;
 
-use crate::{assets::GameAssets, player::CurrentAbility, state::GameState};
+use crate::{
+    abilities::{cooldown::AbilityCooldown, Loadout},
+    assets::GameAssets,
+    player::{CurrentAbility, Player},
+    state::GameState,
+};
 
 pub struct AbilityUiPlugin;
 
 impl Plugin for AbilityUiPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(spawn_ability_ui.in_schedule(OnEnter(GameState::Playing)))
-            .add_system(move_highlight.in_set(OnUpdate(GameState::Playing)))
+            .add_systems((move_highlight, set_highlight_width).in_set(OnUpdate(GameState::Playing)))
             .add_system(despawn_ability_ui.in_schedule(OnExit(GameState::Playing)));
     }
 }
@@ -22,6 +27,8 @@ struct AbilityUiHighlight;
 struct AbilitySlots {
     pub slots: [Entity; 4],
 }
+
+const SELECTOR_WIDTH: f32 = 64.0;
 
 fn spawn_ability_ui(mut commands: Commands, assets: Res<GameAssets>) {
     let style = Style {
@@ -75,18 +82,30 @@ fn spawn_ability_ui(mut commands: Commands, assets: Res<GameAssets>) {
 
     commands.insert_resource(AbilitySlots { slots });
 
-    commands.spawn((
-        AbilityUiHighlight,
-        NodeBundle {
-            style: Style {
-                position: UiRect::new(Val::Px(0.0), Val::Px(0.0), Val::Px(68.0), Val::Auto),
-                size: Size::new(Val::Px(64.0), Val::Px(4.0)),
+    commands
+        .spawn((
+            AbilityUiHighlight,
+            NodeBundle {
+                style: Style {
+                    position: UiRect::new(Val::Px(0.0), Val::Px(0.0), Val::Px(68.0), Val::Auto),
+                    size: Size::new(Val::Px(SELECTOR_WIDTH), Val::Px(4.0)),
+                    overflow: Overflow::Hidden,
+                    ..Default::default()
+                },
+
                 ..Default::default()
             },
-            background_color: Color::WHITE.into(),
-            ..Default::default()
-        },
-    ));
+        ))
+        .with_children(|parent| {
+            parent.spawn(NodeBundle {
+                style: Style {
+                    size: Size::new(Val::Px(SELECTOR_WIDTH), Val::Px(4.0)),
+                    ..Default::default()
+                },
+                background_color: Color::WHITE.into(),
+                ..Default::default()
+            });
+        });
 }
 
 fn despawn_ability_ui(
@@ -108,5 +127,27 @@ fn move_highlight(
         let id = highlight.single();
         commands.entity(id).remove_parent();
         commands.entity(slots.slots[ability.0]).add_child(id);
+    }
+}
+
+fn set_highlight_width(
+    loadout: Query<&Loadout, With<Player>>,
+    mut highlight: Query<&mut Style, With<AbilityUiHighlight>>,
+    selected_ability: Query<&CurrentAbility>,
+    cooldown: Query<Option<&AbilityCooldown>>,
+) {
+    for ability in &selected_ability {
+        let mut style = highlight.single_mut();
+
+        let Ok(loadout) = loadout.get_single() else { continue; };
+        let ability_id = loadout.abilities[ability.0];
+        let cooldown = cooldown.get(ability_id).unwrap();
+
+        if let Some(cooldown) = cooldown {
+            info!("YEAH");
+            style.size.width = Val::Px(SELECTOR_WIDTH * cooldown.percent());
+        } else {
+            style.size.width = Val::Px(SELECTOR_WIDTH);
+        }
     }
 }
